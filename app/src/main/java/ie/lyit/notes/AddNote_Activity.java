9311cc530b5fa66adc3c9ddb14a6a4a44341d261
Barry Kerr <Barry Kerr>
2016-11-23 18:40:30 +0000

    First commit

---
+++ app/src/main/java/ie/lyit/notes/AddNote_Activity.java
@@ -0,0 +1,42 @@
+package ie.lyit.notes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddNote_Activity extends AppCompatActivity {
+
+    private EditText editTextHeader, editTextBody;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note_);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        editTextHeader = (EditText) findViewById(R.id.editTextHeading);
+        editTextBody = (EditText) findViewById(R.id.editTextBody);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: save note
+
+
+                // move to viewNotes
+                startActivity(new Intent(AddNote_Activity.this,ViewNotes_Activity.class));
+
+            }
+        });
+
+
+    }
+
+}
---
+++ app/src/main/java/ie/lyit/notes/ViewNotes_Activity.java
@@ -0,0 +1,13 @@
+package ie.lyit.notes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewNotes_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_notes_);
+    }
+}
